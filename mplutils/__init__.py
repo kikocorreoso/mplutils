6398547dfc2f44bafbaa023c5d0fdc8f6ce9f16c
kikocorreoso <kikocorreoso@gmail.com>
2016-02-26 16:33:26 +0100

    Updated setup.py to update some info

--- mplutils/__init__.py
+++ mplutils/__init__.py
@@ -1,5 +1,9 @@
 from __future__ import absolute_import
 
-from . import text
 from . import colors
 from . import axes
+from . import text
+
+__version__ = '0.1.0'
+__author__ = 'Kiko Correoso'
+__author_email__ = ""
--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, find_packages
+import mplutils
 
-setup(name='mplutils', version='0.1',
-      description='',
-      url='',
-      author='Kiko Correoso',
-      author_email='kikocorreoso@users.noreply.github.com',
-      license='MIT',
-      packages=find_packages(),
-      requires=['matplotlib'],
-      classifiers=['Programming Language :: Python',
-                   'Topic :: Scientific/Engineering :: Visualization',
-                   'Development Status :: 2 - Pre-Alpha',
-                   'License :: OSI Approved :: '
-                   'MIT License'],
+setup(name = 'mplutils', 
+      version = mplutils.__version__,
+      description = '',
+      url = '',
+      author = mplutils.__author__,
+      author_email = mplutils.__author_email__,
+      license = 'MIT',
+      packages = find_packages(),
+      requires = ['numpy', 'matplotlib'],
+      classifiers = ['Programming Language :: Python',
+                     'Topic :: Scientific/Engineering :: Visualization',
+                     'Development Status :: 2 - Pre-Alpha',
+                     'License :: OSI Approved :: '
+                     'MIT License'],
       zip_safe=True)
